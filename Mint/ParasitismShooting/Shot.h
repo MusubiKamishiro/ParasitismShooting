b16c4f79b20ccf0d7644befbd6d0b07893ea93d2
Mint <a>
2019-04-24 12:42:04 +0900

    ショットのパターン増加

--- Mint/ParasitismShooting/Shot.h
+++ Mint/ParasitismShooting/Shot.h
@@ -3,14 +3,14 @@
 #include <memory>
 #include <vector>
 
-const int SHOT_MAX = 2000;
+const int SHOT_MAX = 20000;
 
 class Player;
 class Peripheral;
 
 enum SHOT_PTN
 {
-	NON,
+	WEAK,
 	NORMAL,
 	SHOTGUN,
 	TRACKING,
@@ -20,14 +20,22 @@ enum SHOT_PTN
 
 };
 
+enum MOVE_PTN
+{
+	FAST,
+	SLOW,
+	MAX
+};
+
 typedef struct ShotST
 {
 	int flag;
 	Vector2 pos;
 	Vector2 vel;
 	Vector2 Dir;
+	int movePtn;
 	int level;
-	int ptn;
+	int shotPtn;
 }shot_st;
 
 class Shot
@@ -51,15 +59,26 @@ private:
 
 	shot_st cShot[SHOT_MAX];
 
+	int NormalPosPtnX[4];
+	int NormalPosPtnY[4];
+	int ShotGunPosPtnX[4];
+	int ShotGunPosPtnY[4];
+	int TrackingPosPtnX[4];
+	int TrackingPosPtnY[4];
+	int RadiationPosPtnX[4];
+	int RadiationPosPtnY[4];
+	int LaserPosPtnX[4];
+	int LaserPosPtnY[4];
+
 public:
 	Shot();
 	~Shot();
 
 	void Update();
-	void cSHOT(Vector2 pos);
+	void cSHOT(Vector2 pos,int shotPtn);
 	void Draw(void);
 private:
-	void setBullet(Vector2 pos,	Vector2 vel,Vector2 Dir,int level,int ptn);
+	void setBullet(Vector2 pos,	Vector2 vel,Vector2 Dir,int movePtn,int level,int shotPtn);
 	int SearchBullet(void);
 	void OutofScreen(void);
 };
