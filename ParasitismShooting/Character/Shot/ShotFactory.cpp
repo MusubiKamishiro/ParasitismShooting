b9fde2480557354bbf8dab9f60795342b75ccc5d
Mint <a>
2019-05-13 10:42:38 +0900

    Factoryの実用化

--- ParasitismShooting/Character/Shot/ShotFactory.cpp
+++ ParasitismShooting/Character/Shot/ShotFactory.cpp
@@ -34,70 +34,62 @@ ShotFactory::~ShotFactory()
 {
 }
 
-Shot * ShotFactory::Create(const char * shotname,Vector2f pos, float angle, int Speed, int movePtn, int level, int shotPtn, int shooter)
+Shot * ShotFactory::Create(const char * shotname, Vector2f pos, float angle, int Speed, int movePtn, int level, int shotPtn, int shooter)
 {
-	if(originalShot.find(shotname) != originalShot.end())
+	if (originalShot.find(shotname) != originalShot.end())
 	{
+		for (int j = 0; j < level; j++)
+		{
 		auto shot = originalShot[shotname]->Clone();
-		if (shotname == "ShotNormal")
-		{
-			shot->pos = pos;
-			/*shot->pos = { pos.x - 10 + NormalPosPtnX[cnt],pos.y + NormalPosPtnY[cnt] };*/
-			shot->angle = -M_PI_2;
-		/*	cnt++;
-			if (cnt == level)
+			if (shotname == "ShotNormal")
 			{
-				cnt = 0;
-			}*/
-		}
-		else if (shotname == "ShotRadiation")
-		{
-			shot->pos = pos;
+				//shot->pos = pos;
 
-			shot->angle = -(angle + M_PI_2 / (level / 4)  * cnt);
-			cnt++;
-			if (cnt == level)
+				shot->pos = { pos.x - 10 + NormalPosPtnX[j],pos.y + NormalPosPtnY[j] };
+				shot->angle = -M_PI_2;
+
+			}
+			else if (shotname == "ShotRadiation")
 			{
-				cnt = 0;
+				shot->pos = pos;
+
+				shot->angle = -(angle + M_PI_2 / (level / 4)  * j);
+			}
+			else if (shotname == "ShotRandom")
+			{
+				std::random_device rd;
+				std::mt19937 mt(rd());
+
+				std::uniform_int_distribution<int> rand(1, 10);
+				shot->pos = { pos.x - (float)(rand(mt) * M_PI * 2) + 10,pos.y - 30 };
+				shot->angle = angle;
+			}
+			else if (shotname == "ShotShotgun")
+			{
+				shot->pos = pos;
+				shot->angle = -(((angle + (M_PI / level))  * cnt) + M_PI / 6);
+			}
+			else if (shotname == "ShotTracking")
+			{
+				for (int j = 0; j < level; j++)
+				{
+					shot->pos = { pos.x + 20 * j,pos.y };
+				}
+				shot->angle = angle;
+			}
+			shot->cneterPos = pos;
+			shot->Speed = Speed;
+			shot->movePtn = movePtn;
+			shot->level = level;
+			shot->shotPtn = shotPtn;
+			shot->shooter = shooter;
+			legion.push_back(shot);
+
+			if (j == level)
+			{
+				return shot;
 			}
 		}
-		else if (shotname == "ShotRandom")
-		{
-			std::random_device rd;
-			std::mt19937 mt(rd());
-
-			std::uniform_int_distribution<int> rand(1, 10);
-			shot->pos = { pos.x - (float)(rand(mt) * M_PI * 2) + 10,pos.y - 30 };
-			shot->angle = angle;
-		}
-		else if (shotname == "ShotShotgun")
-		{
-			shot->pos = pos;
-			
-			shot->angle = -(((angle + (M_PI / level))  * cnt) + M_PI / 6);
-			cnt++;
-			if (cnt == level)
-			{
-				cnt = 0;
-			}
-		}
-		else if (shotname == "ShotTracking")
-		{
-			for (int j = 0; j < level; j++)
-			{
-				shot->pos = { pos.x + 20 * j,pos.y };
-			}
-			shot->angle = angle;
-		}
-		shot->cneterPos = pos;
-		shot->Speed = Speed;
-		shot->movePtn = movePtn;
-		shot->level = level;
-		shot->shotPtn = shotPtn;
-		shot->shooter = shooter;
-		legion.push_back(shot);
-
-		return shot;
 	}
 
 	return nullptr;
